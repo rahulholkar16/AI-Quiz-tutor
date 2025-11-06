d66aa5b77377ef60d88c261eba65ce0505398382
Rahul Pal <usefulltech1819@gmail.com>
2025-11-06 23:11:52 +0530

    some changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import FeaturesSection from "./components/FeatureSection";
+// import HeroSection from "./components/HeroSection";
+// import FeaturesSection from "./components/FeatureSection";
 import Footer from "./components/Footer";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App: React.FC = () => {
-  return (
-    <div className="relative flex min-h-screen w-full flex-col bg-[#f6f6f8] dark:bg-[#101022] text-[#d1d1f7] overflow-x-hidden font-display">
-      <Navbar />
-      <main className="grow">
+    return (
+        <div className="relative flex min-h-screen w-full flex-col bg-[#f6f6f8] dark:bg-[#101022] text-[#d1d1f7] overflow-x-hidden font-display">
+            <Navbar />
+            {/* <main className="grow">
         <HeroSection />
         <FeaturesSection />
-      </main>
-      <Footer />
-    </div>
-  );
+      </main> */}
+            <Router>
+                <Routes>
+                    <Route path="/SignUp" element={} />
+                    <Route path="/Login" element={} />
+                    <Route path="/QuizAttempt" element={} />
+                    <Route path="/Home" element={} />
+                    <Route path="/Performane" element={} />
+                    <Route path="/Profile" element={} />
+                    <Route path="/Quiz" element={} />
+                </Routes>
+            </Router>
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
